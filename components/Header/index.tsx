3c572283f99ceed095e588c2a37baed1522e7a03
ramrachai <ramrachaim@gmail.com>
2024-11-02 16:53:53 +0600

    hero text updated

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -156,17 +156,10 @@ const Header = () => {
             <ThemeToggler />
 
             <Link
-              href="https://github.com/NextJSTemplates/solid-nextjs"
-              className="text-regular font-medium text-waterloo hover:text-primary"
-            >
-              GitHub Repo 🌟
-            </Link>
-
-            <Link
-              href="https://nextjstemplates.com/templates/solid"
+              href="/auth/signin"
               className="flex items-center justify-center rounded-full bg-primary px-7.5 py-2.5 text-regular text-white duration-300 ease-in-out hover:bg-primaryho"
             >
-              Get Pro 🔥
+              Login 
             </Link>
           </div>
         </div>
